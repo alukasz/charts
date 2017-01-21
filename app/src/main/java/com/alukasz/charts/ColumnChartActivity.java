26076805ddd238a81caf04f279c04f453258efe5
Łukasz Antończyk <antonczyklukasz@gmail.com>
2017-01-21 20:59:53 +0100

    Add input parsing

--- app/src/main/java/com/alukasz/charts/ColumnChartActivity.java
+++ app/src/main/java/com/alukasz/charts/ColumnChartActivity.java
@@ -2,12 +2,47 @@ package com.alukasz.charts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ColumnChartActivity extends AppCompatActivity {
+    private int[][] chartValues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_column_chart);
+
+        Button btnDraw = (Button) findViewById(R.id.btnDraw);
+        btnDraw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText textChartValues = (EditText) findViewById(R.id.textChartValues);
+                if (parseChartValues(textChartValues.getText().toString()));
+                {
+                    // run sth
+                }
+            }
+        });
+    }
+
+    private boolean parseChartValues(String values)
+    {
+        String[] pairsOfValues = values.split(",");
+        chartValues = new int[pairsOfValues.length][2];
+        for (int i = 0; i < pairsOfValues.length; i++) {
+
+            String[] pair = pairsOfValues[i].split(":");
+            try {
+                chartValues[i][0] = Integer.valueOf(pair[0]);
+                chartValues[i][1] = Integer.valueOf(pair[1]);
+            } catch (Exception e) {
+                Toast.makeText(this, "Failed to parse chart values. Please check data.", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }
+        return true;
     }
 }
