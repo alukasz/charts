4184d8e82e22400f40453895939a458791cdda5e
Łukasz Antończyk <antonczyklukasz@gmail.com>
2017-01-21 20:22:01 +0100

    Initialize project

---
+++ app/src/main/java/com/alukasz/charts/ColumnChartActivity.java
@@ -0,0 +1,13 @@
+package com.alukasz.charts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ColumnChartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_column_chart);
+    }
+}
